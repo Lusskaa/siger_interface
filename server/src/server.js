e8609d5b1708fd738f8e97afcd086e91bfda530a
Lucas Martins <lucasmartins2809@gmail.com>
2022-05-02 22:27:59 -0300

    xxxxCc

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 import app from './app'
 // só posso fazer isso aqui se adicionar o sucrase yarn add sucrase
-app.listen(8000)
+app.listen(process.env.PORT || 8000)
 
 // para rodar  yarn sucrase-node src/server.js
